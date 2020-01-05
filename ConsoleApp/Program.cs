d1dc660b071268a440009e9d9427c5afee732dc4
Solly <mail4solly@gmail.com>
2020-01-05 20:24:21 +1000

    code update

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,26 +1,22 @@
-﻿using ConsoleApp.Common;
-using ConsoleApp.Models;
-using ConsoleApp.Services;
+﻿using ConsoleApp.Models;
 using EasyConsoleCore;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.IO;
 
 namespace ConsoleApp
 {
     class Program
     {
         static bool exit = false;
-        static readonly Menu menu = new Menu()
+        static Menu menu;
+
+        static void Run()
+        {
+            menu = new Menu()
             .Add("Revese Words", () => { MenuAction.Reverse_Words(); })
             .Add("Linked List", () => { MenuAction.Linked_List(); })
-            .Add("Is Palindrome?", () => { MenuAction.IsPalindromeTest(); })
+            .Add("Is Palindrome", () => { MenuAction.IsPalindromeTest(); })
             .Add("On Property Change", () => { MenuAction.ProductPropertyChanged(); })
             .Add("Fibonatchi", () => { MenuAction.Fibonatchi(); })
-            .Add("Integer To Roman", () => { MenuAction.IntegerToRoman(); })
+            .Add("Integer to Roman", () => { MenuAction.IntegerToRoman(); })
             .Add("Longest Word", () => { MenuAction.LongestWord(); })
             .Add("Custom Sort", () => { MenuAction.CustomSort(); })
             .Add("Lottery", () => { MenuAction.Lotto(); })
@@ -30,16 +26,20 @@ namespace ConsoleApp
             .Add("Youtube Data API", () => { MenuAction.Youtube_SearchQuery(); })
             .Add("Facebook Public Profile", () => { MenuAction.Facebook_Profile(); })
             .Add("Facebook Search Query", () => { MenuAction.Facebook_SearchQuery(); })
-            .Add("Exit", () => exit = true);
-
-        static void Main()
-        {
-            Lovely.Test();
+            .Add("Exit", () => { exit = true; });
 
             while (!exit)
             {
                 menu.Display();
+                System.Console.WriteLine("");
             }
+
+            Lovely.Test();
+        }
+
+        static void Main()
+        {
+            Run();
         }
     }
 }
