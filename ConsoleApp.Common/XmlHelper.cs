fc98159b7eec35a8ba22e97d6a15337976dd3dc3
Solly <mail4solly@gmail.com>
2020-05-28 09:24:05 +1000

    add xmlHelper

---
+++ ConsoleApp.Common/XmlHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ConsoleApp.Common
+{
+    public static class XmlHelper
+    {
+        public static IEnumerable<string> FolderNames(string xml, char startingLetter)
+        {
+            var doc = XDocument.Parse(xml);
+            var folders = doc.Descendants("folder");
+            var query = from f in folders
+                        where f.Attribute("name").Value.StartsWith(startingLetter.ToString())
+                        select f.Attribute("name").Value;
+
+            return query;
+        }
+    }
+}
