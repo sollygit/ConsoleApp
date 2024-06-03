73fcd9a3b133e60a8104593959a3dc84b04828f1
Solly <post2solly@gmail.com>
2024-06-03 17:40:17 +1000

    Update usings

--- ConsoleApp.Common/Utility.cs
+++ ConsoleApp.Common/Utility.cs
@@ -3,9 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
-using Google.Apis.YouTube.v3.Data;
+using System.Threading.Tasks;
 
 namespace ConsoleApp.Common
 {
